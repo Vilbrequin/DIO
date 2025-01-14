f20c43fd7d6312bee4af60d2e0f1c462daa08e03
Vilbrequin <hussthedoo@gmail.com>
2025-01-14 14:34:41 +0000

    Add Header files:
    Inc/Dio_Types.h: Contains type definitions of the Dio Module
    Inc/Dio.h: Contains function prototypes and pre-process MICROS

---
+++ Inc/Dio.h
@@ -0,0 +1,203 @@
+#ifndef DIO_H_
+#define DIO_H_
+
+
+/****************************************************************************************************/
+/*                                      Includes                                                    */
+/****************************************************************************************************/
+#include "Dio_Types.h"
+
+
+/****************************************************************************************************/
+/*                                      DioGeneral                                                  */
+/****************************************************************************************************/
+
+/****************************************************************************************************
+ * SWS Item: [ECUC_Dio_00142]
+ * Parameter Name: DioDevErrorDetect
+ * Parent Container: DioGeneral
+ * Description: Switches the development error detection and notification on or off:
+ *              - true: detection and notification is enabled.
+ *              - false: detection and notification is disabled.
+ * Multiplicity: 1
+ * Default value: false
+ * scope: local
+ ****************************************************************************************************/
+#define DIO_DEV_ERROR_DETECT            (STD_OFF)
+
+/****************************************************************************************************
+ * SWS Item: [ECUC_Dio_00153]
+ * Parameter Name: DioFlipChannelApi
+ * Parent Container: DioGeneral
+ * Description: Adds / removes the service Dio_FlipChannel() from the code.
+ * Multiplicity: 1
+ * Default value: --
+ * scope: local
+ ****************************************************************************************************/
+#define DIO_FLIP_CHANNE_LAPI            (STD_OFF)           
+
+/****************************************************************************************************
+ * SWS Item: [ECUC_Dio_00155]
+ * Parameter Name: DioMaskedWritePortApi
+ * Parent Container: DioGeneral
+ * Description: Adds / removes the service Dio_MaskedWritePort() from the code.
+ * Multiplicity: 1
+ * Default value: false
+ * scope: local
+ ****************************************************************************************************/
+#define DIO_MASKED_WRITE_PORT_API       (STD_OFF)
+
+/****************************************************************************************************
+ * SWS Item: [ECUC_Dio_00143]
+ * Parameter Name: DioVersionInfoApi
+ * Parent Container: DioGeneral
+ * Description: Adds / removes the service Dio_GetVersionInfo() from the code.
+ * Multiplicity: 1
+ * Default value: false
+ * scope: local
+ ****************************************************************************************************/
+#define DIO_VERSION_INF_OAPI            (STD_OFF)
+
+/****************************************************************************************************/
+/*                                      DioGeneral                                                  */
+/****************************************************************************************************/
+
+/****************************************************************************************************
+ * Service Name: Dio_ReadChannel
+ * Sws_Index : 8.3.1 [SWS_Dio_00133]
+ * Service ID[hex]:0x00
+ * Sync/Async: Synchronous
+ * Reentrancy: reentrant
+ * Parameters (in): ChannelId - ID of DIO channel
+ * Parameters (inout): None
+ * Parameters (out): None
+ * Return value: Dio_LevelType :
+ *              - STD_HIGH The physical level of the corresponding Pin is STD_HIGH
+ *              - STD_LOW The physical level of the corresponding Pin is STD_LOW
+ * Description: Returns the value of the specified DIO channel.
+****************************************************************************************************/
+Dio_LevelType Dio_ReadChannel (Dio_ChannelType ChannelId);
+
+/****************************************************************************************************
+ * Service Name: Dio_WriteChannel
+ * Sws_Index : 8.3.2 [SWS_Dio_00134]
+ * Service ID[hex]:0x01
+ * Sync/Async: Synchronous
+ * Reentrancy: reentrant
+ * Parameters (in): - ChannelId - ID of DIO channel
+ *                  - Level - Value to be written
+ * Parameters (inout): None
+ * Parameters (out): None
+ * Return value: None
+ * Description: Service to set a level of a channel.
+****************************************************************************************************/
+void Dio_WriteChannel (Dio_ChannelType ChannelId, Dio_LevelType Level);
+
+/****************************************************************************************************
+ * Service Name: Dio_ReadPort
+ * Sws_Index : 8.3.3 [SWS_Dio_00135]
+ * Service ID[hex]:0x02
+ * Sync/Async: Synchronous
+ * Reentrancy: reentrant
+ * Parameters (in): PortId - ID of DIO Port
+ * Parameters (inout): None
+ * Parameters (out): None
+ * Return value: Dio_PortLevelType - Level of all channels of that port
+ * Description: Returns the level of all channels of that port.
+****************************************************************************************************/
+Dio_PortLevelType Dio_ReadPort (Dio_PortType PortId);
+
+/****************************************************************************************************
+ * Service Name: Dio_WritePort
+ * Sws_Index : 8.3.4 [SWS_Dio_00136]
+ * Service ID[hex]:0x03
+ * Sync/Async: Synchronous
+ * Reentrancy: reentrant
+ * Parameters (in): - PortId - ID of DIO Port
+ *                  - Level - Value to be written
+ * Parameters (inout): None
+ * Parameters (out): None
+ * Return value: None
+ * Description: Service to set a value of the port.
+****************************************************************************************************/
+void Dio_WritePort (Dio_PortType PortId, Dio_PortLevelType Level);
+
+/****************************************************************************************************
+ * Service Name: Dio_ReadChannelGroup
+ * Sws_Index : 8.3.5 [SWS_Dio_00137]
+ * Service ID[hex]:0x04
+ * Sync/Async: Synchronous
+ * Reentrancy: reentrant
+ * Parameters (in): ChannelGroupIdPtr - Pointer to ChannelGroup
+ * Parameters (inout): None
+ * Parameters (out): None
+ * Return value: Dio_PortLevelType Level of a subset of the adjoining bits of a port
+ * Description: This Service reads a subset of the adjoining bits of a port.
+****************************************************************************************************/
+Dio_PortLevelType Dio_ReadChannelGroup (const Dio_ChannelGroupType* ChannelGroupIdPtr);
+
+/****************************************************************************************************
+ * Service Name: Dio_WriteChannelGroup
+ * Sws_Index : 8.3.6 [SWS_Dio_00138]
+ * Service ID[hex]:0x05
+ * Sync/Async: Synchronous
+ * Reentrancy: reentrant
+ * Parameters (in): - ChannelGroupIdPtr - Pointer to ChannelGroup
+ *                  - Level - Value to be written
+ * Parameters (inout): None
+ * Parameters (out): None
+ * Return value: None
+ * Description: Service to set a subset of the adjoining bits of a port to a specified level
+****************************************************************************************************/
+void Dio_WriteChannelGroup (const Dio_ChannelGroupType* ChannelGroupIdPtr, Dio_PortLevelType Level);
+
+/****************************************************************************************************
+ * Service Name: Dio_GetVersionInfo
+ * Sws_Index : 8.3.7 [SWS_Dio_00139]
+ * Service ID[hex]:0x12
+ * Sync/Async: Synchronous
+ * Reentrancy: reentrant
+ * Parameters (in): None
+ * Parameters (inout): None
+ * Parameters (out): VersionInfo - Pointer to where to store the version information of this module
+ * Return value: None
+ * Description: Service to get the version information of this module.
+****************************************************************************************************/
+void Dio_GetVersionInfo (Std_VersionInfoType* VersionInfo);
+
+/****************************************************************************************************
+ * Service Name: Dio_FlipChannel
+ * Sws_Index : 8.3.8 [SWS_Dio_00190]
+ * Service ID[hex]:0x11
+ * Sync/Async: Synchronous
+ * Reentrancy: reentrant
+ * Parameters (in): ChannelId - ID of DIO channel
+ * Parameters (inout): None
+ * Parameters (out):None
+ * Return value: Dio_LevelType :
+ *              - STD_HIGH The physical level of the corresponding Pin is STD_HIGH
+ *              - STD_LOW The physical level of the corresponding Pin is STD_LOW
+ * Description: Service to flip (change from 1 to 0 or from 0 to 1) the level of a channel and 
+ *              return the level of the channel after flip.
+****************************************************************************************************/
+Dio_LevelType Dio_FlipChannel (Dio_ChannelType ChannelId);
+
+
+/****************************************************************************************************
+ * Service Name: Dio_MaskedWritePort
+ * Sws_Index : 8.3.9 [SWS_Dio_00300]]
+ * Service ID[hex]:0x13
+ * Sync/Async: Synchronous
+ * Reentrancy: reentrant
+ * Parameters (in): - PortId - ID of DIO Port
+ *                  - Level - Value to be written
+ *                  - Mask - Channels to be masked in the port
+ * Parameters (inout): None
+ * Parameters (out):None
+ * Return value: None
+ * Description: Service to set the value of a given port with required mask.
+****************************************************************************************************/
+void Dio_MaskedWritePort (Dio_PortType PortId, Dio_PortLevelType Level, Dio_PortLevelType Mask);
+
+
+#endif /* DIO_H_ */
---
+++ Inc/Dio_Types.h
@@ -0,0 +1,79 @@
+#ifndef DIO_TYPES_H_
+#define DIO_TYPES_H_
+
+/****************************************************************************************************/
+/*                                      Includes                                                    */
+/****************************************************************************************************/
+#include "Std_Types.h"
+
+/****************************************************************************************************/
+/*                                      Macros Definitions                                          */
+/****************************************************************************************************/
+#define DIO_CONFIGURED_CHANNELS     (89U)
+#define DIO_CONFIGURED_PORTS        (5U) /* Port A to Port E*/
+#define DIO_CONFIGURED_GROUPS       (3U) /* eg of configured groups (3 groups)*/
+
+/****************************************************************************************************/
+/*                                  [SWS_Dio_91001] Development Errors                              */
+/****************************************************************************************************/
+
+#define DIO_E_PARAM_INVALID_CHANNEL_ID          0x0A
+
+#define DIO_E_PARAM_INVALID_PORT_ID             0x14
+
+#define DIO_E_PARAM_INVALID_GROUP               0x1F
+
+#define DIO_E_PARAM_POINTER                     0x20
+/****************************************************************************************************/
+/*                                      Types Definitions                                           */
+/****************************************************************************************************/
+
+/*
+ * Index : 8.2.1 [SWS_Dio_00182]
+ * Description : Numeric ID of a DIO channel.
+ */
+typedef uint8 Dio_ChannelType;
+
+/*
+ * Index : 8.2.2 [SWS_Dio_00183]
+ * Description : Numeric ID of a DIO port.
+ */
+typedef uint8 Dio_PortType;
+
+/*
+ * Index : 8.2.5 [SWS_Dio_00186]
+ * Description : If the μC owns ports of different port widths (e.g. 4, 8,16...Bit) Dio_PortLevelType 
+ *              inherits the size of the largest port.
+ */
+typedef uint8 Dio_PortLevelType;
+
+/*
+ * Index : 8.2.4 [SWS_Dio_00185]
+ * Description : These are the possible levels a DIO channel can have (input or output).
+ */
+typedef uint8 Dio_LevelType;
+#define STD_LOW                 (Dio_LevelType)0x00
+#define STD_HIGH                (Dio_LevelType)0x01
+
+/*
+ * Index : 8.2.3 [SWS_Dio_00184]
+ * Description : Channel group, which consists of several adjoining channels within a port.
+ */
+typedef struct {
+    uint8 mask;
+    uint8 offset;
+    Dio_PortType port;
+}Dio_ChannelGroupType;
+
+typedef struct{
+    Dio_PortType portId;
+    Dio_ChannelType channelId;
+}Dio_ConfigChannelType;
+
+typedef struct{
+    Dio_ConfigChannelType channels[DIO_CONFIGURED_CHANNELS];
+    Dio_PortType ports[DIO_CONFIGURED_PORTS];
+    Dio_ChannelGroupType groups[DIO_CONFIGURED_GROUPS];
+}Dio_ConfigType;
+
+#endif /* DIO_TYPES_H_ */
