30700d98e693b9a5fc834b4e40c3afca74734542
Vilbrequin <hussthedoo@gmail.com>
2025-01-18 22:45:00 +0000

    Read APIs only read Input registers
    Change the Dio_ReadChannel API implemnetation, by reading only the PDIR
    corresoponding pin.
    
    Implemenet Dio_ReadPort API

--- Src/Dio.c
+++ Src/Dio.c
@@ -33,69 +33,37 @@ void Dio_Init(Dio_ConfigType *ConfigPtr){
 ****************************************************************************************************/
 Dio_LevelType Dio_ReadChannel (Dio_ChannelType ChannelId){
 
-    volatile uint32 *port_ddr = NULL_PTR;
     volatile uint32 *port_dir = NULL_PTR;
-    volatile uint32 *port_dor = NULL_PTR;
 
     switch (Dio_PortChannels[ChannelId].portId)
     {
     case PORT_A_ID:
-        port_ddr = (volatile uint32 *)GPIO_A_PDDR;
         port_dir = (volatile uint32 *)GPIO_A_PDIR;
-        port_dor = (volatile uint32 *)GPIO_A_PDOR;
         break;
     case PORT_B_ID:
-        port_ddr = (volatile uint32 *)GPIO_B_PDDR;
         port_dir = (volatile uint32 *)GPIO_B_PDIR;
-        port_dor = (volatile uint32 *)GPIO_B_PDOR;
         break;
     case PORT_C_ID:
-        port_ddr = (volatile uint32 *)GPIO_C_PDDR;
         port_dir = (volatile uint32 *)GPIO_C_PDIR;
-        port_dor = (volatile uint32 *)GPIO_C_PDOR;
         break;
     case PORT_D_ID:
-        port_ddr = (volatile uint32 *)GPIO_D_PDDR;
         port_dir = (volatile uint32 *)GPIO_D_PDIR;
-        port_dor = (volatile uint32 *)GPIO_D_PDOR;
         break;
     case PORT_E_ID:
-        port_ddr = (volatile uint32 *)GPIO_E_PDDR;
         port_dir = (volatile uint32 *)GPIO_E_PDIR;
-        port_dor = (volatile uint32 *)GPIO_E_PDOR;
         break;
     default:
         /* Do Nothing */
         break;
     }
 
-    if(BIT_IS_SET(*(volatile uint32 *) port_ddr, Dio_PortChannels[ChannelId].channelId)){
-        /* Pin is configured as an Output*/
-        if(BIT_IS_SET(*(volatile uint32 *) port_dor, Dio_PortChannels[ChannelId].channelId)){
-            return STD_HIGH;
-        }
-        else if(BIT_IS_CLEAR(*(volatile uint32 *) port_dor, Dio_PortChannels[ChannelId].channelId)){
-            return STD_LOW;
-        }
-        else {
-            /* Do Nothing*/
-        }
+    if(BIT_IS_SET(*(volatile uint32 *) port_dir, Dio_PortChannels[ChannelId].channelId)){
+        return STD_HIGH;
     }
-    else if(BIT_IS_CLEAR(*(volatile uint32 *) port_ddr, Dio_PortChannels[ChannelId].channelId)){
-        /* Pin is configured as an Input*/
-        if(BIT_IS_SET(*(volatile uint32 *) port_dir, Dio_PortChannels[ChannelId].channelId)){
-            return STD_HIGH;
-        }
-        else if(BIT_IS_CLEAR(*(volatile uint32 *) port_dir, Dio_PortChannels[ChannelId].channelId)){
-            return STD_LOW;
-        }
-        else {
-            /* Do Nothing*/
-        }
+    else if(BIT_IS_CLEAR(*(volatile uint32 *) port_dir, Dio_PortChannels[ChannelId].channelId)){
+        return STD_LOW;
     }
-    else {
-        /* Do Nothing*/
-    }
+
 }
 
 /****************************************************************************************************
@@ -110,7 +78,7 @@ Dio_LevelType Dio_ReadChannel (Dio_ChannelType ChannelId){
  * Parameters (out): None
  * Return value: None
  * Description: Service to set a level of a channel.
-****************************************************************************************************/
+ ****************************************************************************************************/
 void Dio_WriteChannel (Dio_ChannelType ChannelId, Dio_LevelType Level){
     volatile uint32 *port_ddr = NULL_PTR;
     volatile uint32 *port_dor = NULL_PTR;
@@ -156,4 +124,49 @@ void Dio_WriteChannel (Dio_ChannelType ChannelId, Dio_LevelType Level){
     else{
         /* Do Nothing */
     }
-}   
+}
+
+/****************************************************************************************************
+ * Service Name: Dio_ReadPort
+ * Sws_Index : 8.3.3 [SWS_Dio_00135]
+ * Service ID[hex]:0x02
+ * Sync/Async: Synchronous
+ * Reentrancy: reentrant
+ * Parameters (in): PortId - ID of DIO Port
+ * Parameters (inout): None
+ * Parameters (out): None
+ * Return value: Dio_PortLevelType - Level of all channels of that port
+ * Description: Returns the level of all channels of that port.
+****************************************************************************************************/
+Dio_PortLevelType Dio_ReadPort (Dio_PortType PortId){
+
+    volatile uint32 *port_dir = NULL_PTR;
+    Dio_PortLevelType ptr_level = STD_HIGH;
+
+    switch (PortId)
+    {
+    case PORT_A_ID:
+        port_dir = (volatile uint32 *)GPIO_A_PDIR;
+        break;
+    case PORT_B_ID:
+        port_dir = (volatile uint32 *)GPIO_B_PDIR;
+        break;
+    case PORT_C_ID:
+        port_dir = (volatile uint32 *)GPIO_C_PDIR;
+        break;
+    case PORT_D_ID:
+        port_dir = (volatile uint32 *)GPIO_D_PDIR;
+        break;
+    case PORT_E_ID:
+        port_dir = (volatile uint32 *)GPIO_E_PDIR;
+        break;
+    default:
+        /* Do Nothing */
+        break;
+    }
+
+    if(port_dir != NULL_PTR){
+        ptr_level = (Dio_PortLevelType)*port_dir;
+    }
+    return ptr_level;
+}
